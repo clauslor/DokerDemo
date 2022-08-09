500c1fb42ce28f5c26084990fdfd1f13fac46eb4
Lorenz Claus <clauslor@posteo.de>
2022-08-09 11:15:21 +0200

    dump json to redis

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 import time
-
+import json
 import redis
+
 from flask import Flask,redirect,url_for,render_template,request
 
 app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
+
+def dump_json():
+    gps_coords= [
+        {'uuid':'123', 'url':'....', 'x':'52.49015802952927', 'y':'13.415608457497155'},
+        {'uuid':'234', 'url':'....', 'x':'52.490131898467695', 'y':' 13.425682834308834'}
+    ]
+
+    # Convert python dict to JSON str and save to Redis
+    json_gps_coords = json.dumps(gps_coords)
+    r.set('gps_coords', json_gps_coords)
+
+def get_gpscoods():
+    unpacked_gps_coords = json.loads(r.get('gps_coords').decode('utf-8'))
+    gps_coords == unpacked_gps_coords
 
 def get_hit_count():
     retries = 5
@@ -19,6 +34,7 @@ def get_hit_count():
 
 @app.route('/')
 def hello():
+    dump_json()
     count = get_hit_count()
     return render_template('index.html', hits=count)
 
